bbabb95934cd6127848a99b8359cb7f2036d3dfd
Silva <salinasmarinjosejavier20@gmail.com>
2025-12-30 12:29:23 -0500

    Update embed.js

--- pages/api/embed.js
+++ pages/api/embed.js
@@ -1,13 +1,17 @@
 // pages/api/embed.js
 export default function handler(req, res) {
   const { id, season, episode } = req.query;
+
   if (!id) {
     return res.status(400).send('Error: falta ?id=');
   }
-  let playerUrl = `https://jjmovies.lat/watch.html?id=${id}`;
-  if (season) playerUrl += `&season=${season}`;
-  if (episode) playerUrl += `&episode=${episode}`;
+
+  let playerUrl = `https://jjmovies.lat/watch.html?id=${encodeURIComponent(id)}`;
+  if (season) playerUrl += `&season=${encodeURIComponent(season)}`;
+  if (episode) playerUrl += `&episode=${encodeURIComponent(episode)}`;
+
   const iframe = `<iframe class="aspect-video w-full" src="${playerUrl}" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>`;
+
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.status(200).send(iframe);
 }
